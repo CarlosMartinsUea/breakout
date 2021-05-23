c51f546ea9e852e92f1985d688758feba5c9e0f1
Carlos Alberto De Jesus Vasconcelos <cadjv.snf20@uea.edu.br>
2021-05-23 18:53:40 -0400

    Update breakout.py

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -2,53 +2,56 @@ import pygame
 
 pygame.init()
 
+"""Colors used in the game"""
 COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 
+"""Maximum Score Set"""
 SCORE_MAX = 2
 
+"""Game Variables"""
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Breakout - PyGame Edition')
 
-# score text
+"""Score text"""
 score_font = pygame.font.Font('assets/press_start_font.ttf', 44)
 score_text = score_font.render('00 x 00', True, COLOR_WHITE, COLOR_BLACK)
 score_text_rect = score_text.get_rect()
 score_text_rect.center = (680, 50)
 
-# victory text
+"""Victory text"""
 victory_font = pygame.font.Font('assets/press_start_font.ttf', 100)
 victory_text = victory_font .render('VICTORY', True, COLOR_WHITE, COLOR_BLACK)
 victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (450, 350)
 
-# sound effects
+"""Sound effects"""
 bounce_sound_effect = pygame.mixer.Sound('assets/bounce.wav')
 scoring_sound_effect = pygame.mixer.Sound('arcade_bleep_sound.wav')
 
-# player 1
+"""Player 1 Controls and Assets"""
 player_1 = pygame.image.load('assets/player.png')
 player_1_y = 300
 player_1_move_up = False
 player_1_move_down = False
 
-# player 2 - robot
+"""CPU Controls and Assets"""
 player_2 = pygame.image.load('assets/player.png')
 player_2_y = 300
 
-# ball
+"""Game's Ball"""
 ball = pygame.image.load('assets/ball.png')
 ball_x = 640
 ball_y = 360
 ball_dx = 5
 ball_dy = 5
 
-# score
+"""Game's score"""
 score_1 = 0
 score_2 = 0
 
-# game loop
+"""Game loop"""
 game_loop = True
 game_clock = pygame.time.Clock()
 
@@ -58,7 +61,7 @@ while game_loop:
         if event.type == pygame.QUIT:
             game_loop = False
 
-        #  keystroke events
+        """Keystroke events"""
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 player_1_move_up = True
@@ -70,13 +73,13 @@ while game_loop:
             if event.key == pygame.K_DOWN:
                 player_1_move_down = False
 
-    # checking the victory condition
+    """Victory condition"""
     if score_1 < SCORE_MAX and score_2 < SCORE_MAX:
 
-        # clear screen
+        """Clear screen"""
         screen.fill(COLOR_BLACK)
 
-        # ball collision with the wall
+        """Ball collision with the wall"""
         if ball_y > 700:
             ball_dy *= -1
             bounce_sound_effect.play()
@@ -84,21 +87,21 @@ while game_loop:
             ball_dy *= -1
             bounce_sound_effect.play()
 
-        # ball collision with the player 1 's paddle
+        """Ball collision with the player 1's paddle"""
         if ball_x < 100:
             if player_1_y < ball_y + 25:
                 if player_1_y + 150 > ball_y:
                     ball_dx *= -1
                     bounce_sound_effect.play()
 
-        # ball collision with the player 2 's paddle
+        """Ball collision with the CPU's paddle"""
         if ball_x > 1160:
             if player_2_y < ball_y + 25:
                 if player_2_y + 150 > ball_y:
                     ball_dx *= -1
                     bounce_sound_effect.play()
 
-        # scoring points
+        """Scoring points"""
         if ball_x < -50:
             ball_x = 640
             ball_y = 360
@@ -114,52 +117,52 @@ while game_loop:
             score_1 += 1
             scoring_sound_effect.play()
 
-        # ball movement
+        """Ball movement"""
         ball_x = ball_x + ball_dx
         ball_y = ball_y + ball_dy
 
-        # player 1 up movement
+        """Player 1 up movement"""
         if player_1_move_up:
             player_1_y -= 5
         else:
             player_1_y += 0
 
-        # player 1 down movement
+        """Player 1 down movement"""
         if player_1_move_down:
             player_1_y += 5
         else:
             player_1_y += 0
 
-        # player 1 collides with upper wall
+        """Player 1 collides with upper wall"""
         if player_1_y <= 0:
             player_1_y = 0
 
-        # player 1 collides with lower wall
+        # Player 1 collides with lower wall
         elif player_1_y >= 570:
             player_1_y = 570
 
-        # player 2 "Artificial Intelligence"
+        """CPU Artificial Intelligence"""
         player_2_y = ball_y
         if player_2_y <= 0:
             player_2_y = 0
         elif player_2_y >= 570:
             player_2_y = 570
 
-        # update score hud
+        """Update score hud"""
         score_text = score_font.render(str(score_1) + ' x ' + str(score_2), True, COLOR_WHITE, COLOR_BLACK)
 
-        # drawing objects
+        """Drawing objects"""
         screen.blit(ball, (ball_x, ball_y))
         screen.blit(player_1, (50, player_1_y))
         screen.blit(player_2, (1180, player_2_y))
         screen.blit(score_text, score_text_rect)
     else:
-        # drawing victory
+        """Drawing victory"""
         screen.fill(COLOR_BLACK)
         screen.blit(score_text, score_text_rect)
         screen.blit(victory_text, victory_text_rect)
 
-    # update screen
+    """Update screen"""
     pygame.display.flip()
     game_clock.tick(60)
 
